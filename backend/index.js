3641dc17729e689aef1d245599b6d8f8c3d39af3
edwin yue <53625790+edwaddle@users.noreply.github.com>
2025-02-23 16:14:12 -0800

    used gitignore / dotenv to hide finnhub api key (hopefully). And implimented the endpoint /history and wtv Symbol
    Updated post and get to be correct, Post doesn't workk so far, but get should?
    
    need to update the UI so that it matches the github repo

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,28 @@ const cors = require("cors");
 const corsOptions = {
   origin: ["http://localhost:5173"],
 };
+const finnhub = require("finnhub");
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); //necessary for get
+app.use(express.json()); //necessary for post
+const fruits = ["apple", "orange", "banana"];
 
-app.get("/api", (req, res) => {
-  res.json({ fruits: ["apple", "orange", "banana"] });
+app.post("/start-monitoring", (req, res) => {
+  res.json({ fruits: fruits });
+  const { min, sec, symbol } = req.body;
+});
+
+app.get("/history", (req, res) => {
+  api_key.apiKey = "cut7lihr01qrsirl7g50cut7lihr01qrsirl7g5g";
+  const finnhubClient = new finnhub.DefaultApi();
+  const stockSymbol = req.query.symbol;
+  if (!stockSymbol) {
+    return res.status(400).json({ error: "Missing symbol query parameter" });
+  }
+  finnhubClient.quote(stockSymbol, (error, data, response) => {
+    res.json(data);
+  });
 });
 
 app.listen(8080, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import axios from "axios";
 
@@ -8,45 +6,97 @@ function App() {
   const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
 
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api");
-    setArray(response.data.fruits);
-    console.log(response.data.fruits);
+  const monitorStocks = async () => {
+    axios
+      .post("http://localhost:8080/start-monitoring", {
+        min: MIN.current.value,
+        sec: SEC.current.value,
+        symbol: SYMBOL.current.value,
+      })
+      .then(function (req) {
+        console.log("Response:", req.data);
+        retrieveHistory();
+      })
+      .catch(function (error) {
+        console.log("Error:", error.req.data);
+      });
   };
 
   useEffect(() => {
-    fetchAPI();
+    retrieveHistory();
   }, []);
 
+  const MIN = useRef("");
+  const SEC = useRef("");
+  const SYMBOL = useRef("");
+  const retrieveHistory = async () => {
+    axios
+      .get("http://localhost:8080/history", {
+        params: {
+          symbol: SYMBOL.current.value, // Pass symbol as a query parameter
+        },
+      })
+      .then(function (req) {
+        console.log("Response:", req.data);
+        const rowLength = Object.keys(req.data).length;
+        const colLength = 1;
+        const copyArray = Array.from(
+          { length: rowLength },
+          () => new Array(colLength)
+        );
+
+        /*
+        const colLength = Object.keys(req.data[0]).length;
+        const copyArray = [rowLength][colLength];
+        for (let i = 0; i < rowLength; i++) {
+          for (let j = 0; j < colLength; j++) {
+            copyArray[i][j] = req.data[i][j];
+          }
+        }
+          */
+        let i = 0;
+        for (var prop in req.data) {
+          console.log(req.data[prop]);
+          if (req.data[prop] === null) {
+            copyArray[0][i] = "null";
+          } else {
+            copyArray[0][i] = req.data[prop];
+          }
+          i++;
+        }
+        setArray(copyArray);
+      })
+      .catch(function (error) {
+        console.error("Error in retrieving history: " + error);
+      });
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App">
+      <h1>Honk Honk hit the klaxon</h1>
+      <div className="inputGroup">
+        <input ref={MIN} type="text" placeholder="MIN"></input>
+        <input ref={SEC} type="text" placeholder="SEC"></input>
+        <input
+          ref={SYMBOL}
+          type="text"
+          placeholder="SYMBOL"
+          defaultValue="AAPL"
+        ></input>
+        <button id="EnterButton" onClick={monitorStocks}>
+          Enter
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        {array.map((fruit, index) => (
-          <div key={index}>
-            <p>{fruit}</p>
-            <br></br>
-          </div>
-        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <table className="outputGroup">
+        {array.map((row, index) => (
+          <tr key={index}>
+            {row.map((col, index) => (
+              <th key={index}>{col}</th>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
 
